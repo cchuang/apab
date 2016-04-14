2ce5947eb76262a5b55c824037e7d070019c818e
Cheng-Ching Huang <cchuang.tw@gmail.com>
2016-04-14 14:02:35 +0800

    * mysql_db_schema.sql: generated by MySQL Workbench EER Diagram * all: compatible with new db schema

--- auto_increment.sh
+++ auto_increment.sh
@@ -3,6 +3,6 @@ USER=$(awk -F "=" '/user/ {print $2}' cred.ini | tr -d '[[:space:]]')
 PASS=$(awk -F "=" '/password/ {print $2}' cred.ini | tr -d '[[:space:]]')
 
 mysql --user=$USER --password=$PASS apab <<ZZZZFFFFAAAA
-INSERT INTO status (slideno, path) SELECT slideno + 1 AS slideno, path FROM status ORDER BY idstatus DESC LIMIT 1;
-SELECT * FROM status ORDER BY idstatus DESC LIMIT 1;
+INSERT INTO status (slideno, path, event_id, opt_type) SELECT slideno + 1 AS slideno, path, event_id, opt_type FROM status ORDER BY id DESC LIMIT 1;
+SELECT * FROM status ORDER BY id DESC LIMIT 1;
 ZZZZFFFFAAAA
--- event_init.sh
+++ event_init.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
-FILE=$1
 if [[ $# -lt 2 ]]; then
-	START=1
-else
-	START=$2
+	echo "Usage: $0 EVENT_NAME FILE [START_PAGE] [OPT_TYPE]"
+	exit
 fi
+
+EVENT_NAME=$1
+FILE=$2
 
 if [[ $# -lt 3 ]]; then
-	ASSESS='assessment.php'
+	START=1
 else
-	ASSESS=$3
+	START=$3
 fi
 
+if [[ $# -lt 4 ]]; then
+	OPTTYPE='GENLIKE'
+else
+	OPTTYPE=$4
+fi
+
+echo $EVENT_NAME
 echo $FILE
 echo $START
-echo $ASSESS
+echo $OPTTYPE
 
 USER=$(awk -F "=" '/user/ {print $2}' cred.ini | tr -d '[[:space:]]')
 PASS=$(awk -F "=" '/password/ {print $2}' cred.ini | tr -d '[[:space:]]')
 
 mysql --user=$USER --password=$PASS apab <<ZZZZFFFFAAAA
-INSERT INTO status (path, slideno, handle) VALUE ("$FILE", "$START", "$ASSESS")
+UPDATE events SET live=0;
+INSERT INTO events (name, live, default_opt_type) VALUE ("$EVENT_NAME", 1, "$OPTTYPE");
+INSERT INTO status (event_id, path, slideno, opt_type) SELECT id AS event_id, "$FILE" AS path, "$START" AS slideno, "$OPTTYPE" AS opt_type FROM events WHERE live=1;
 ZZZZFFFFAAAA
 
